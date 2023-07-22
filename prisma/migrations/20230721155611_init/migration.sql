9b1b483c316adc70e3875d9252d7565156cb1318
ShahiryarArif <shahiryararif@gmail.com>
2023-07-22 16:10:02 +0500

    Next 13 Todo app with prisma

---
+++ prisma/migrations/20230721155611_init/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - Made the column `updatedAt` on table `Todo` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "complete" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Todo" ("complete", "createdAt", "id", "title", "updatedAt") SELECT "complete", "createdAt", "id", "title", "updatedAt" FROM "Todo";
+DROP TABLE "Todo";
+ALTER TABLE "new_Todo" RENAME TO "Todo";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
